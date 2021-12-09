1e455011ed9a59284c764039b3d107146734cba2
Boris Alennikov <AlennikovBoris@gmail.com>
2021-12-09 10:03:22 +0300

    changed main

--- main.cpp
+++ main.cpp
@@ -12,18 +12,20 @@ int main() {
     stack << test;
 
     stack.push(3);
-
     stack.push(12);
     stack.push(155);
+    stack.push(18);
+    stack.push(-133);
+    stack.push(172.7882);
+    stack.push(7227);
 
+    std::cout << "Your stack:\n";
     stack.print(std::cout);
 
-    std::cout << "\nTop element " << stack.top();
 
-    std::cout << "\n";
-    stack.pop();
-    stack.print(std::cout);
-
+    std::cout << "\nTop element " << stack.top() << std::endl;
+    std::cout << "Stack size: " << stack.getSize() << std::endl;
+    std::cout << "Stack fourth element: " << stack[5] << std::endl;
 
     return 0;
 }
